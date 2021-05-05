882a5742859f760d464124c880b8b29daf757eaf
Juraj Lapčák <55839515+georgeHeishi@users.noreply.github.com>
2021-05-05 19:58:48 +0200

    integrity fixed (#2)
    Co-authored-by: Blochin <sevcik.lubomir02@gmail.com>

--- otazka/script/script.js
+++ otazka/script/script.js
@@ -1,21 +1,28 @@
 let question = document.getElementById("question");
 let cnt_answers = document.getElementById("cnt-answers");
+let submit = document.getElementById("submit-question");
 
 cnt_answers.addEventListener("change",function (){
     let cnt = this.value;
     let target = document.getElementById("target");
     target.innerHTML = "";
     for(let i =0; i < cnt; i++){
-        let source =    "<label for="+i+"_question"+"></label>"+"<input type='text' class='question' id="+i+"_question"+"/></label>"+
-                        "<label for="+i+"_correct"+" ></label>"+"<input type='radio' class='correct' id="+i+"_correct"+"/></label>";
+        let source =    "<label for="+i+"_question"+"></label>"+"<input type='text' class='answer' id='"+i+"_answer"+"'/></label>"+
+                        "<label for="+i+"_correct"+" ></label>"+"<input type='radio' class='correct' id='"+i+"_correct"+"'/></label>";
 
         target.innerHTML = target.innerHTML + source ;
     }
-    for(let i = 0; i < cnt; i++){
-        let questions = document.getElementsByClassName("question");
-        let answers = document.getElementsByClassName("correct");
 
-
-
-    }
 })
+
+submit.addEventListener("click",function (){
+    let answers = document.getElementsByClassName("answer");
+    let correct_answer = document.getElementsByClassName("correct");
+
+    Array.from(answers).forEach(function (item){
+        let id_answer = item.id.split("_");
+        console.log(id_answer);
+
+    });
+})
+

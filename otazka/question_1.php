507cccf9d90a64ee883ae238ad746bdefd93f4c0
Juraj Budai <74743305+nethy-x@users.noreply.github.com>
2021-05-05 23:31:43 +0200

    Budai/otazka
    * otazka1
    * otazka3

---
+++ otazka/question_1.php
@@ -0,0 +1,26 @@
+<form>
+    <label for="question1" ></label>
+    <input id="question1" type="text">
+
+    <label for="cnt-answers1"></label>
+    <select id="cnt-answers1">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+    </select>
+    <div id="target1">
+
+    </div>
+
+    <label for="submit-question1"></label>
+    <button id="submit-question1" name="submit-question1" >Submit</button>
+
+
+</form>
+<script src="script/question_1_script.js"></script>
+<?php

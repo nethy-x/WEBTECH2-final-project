3438d5f559fee5709f0025eaa0a4aab29ba5ef80
Marek Kacmar <kacmar.marek@gmail.com>
2021-05-10 11:09:00 +0200

    database connection updated

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
 const DB_HOST = 'localhost';
-const DB_USER = 'xlapcak';
-const DB_PASS = 'Koniecstringuje\0';
+const DB_USER = 'xkacmarm';
+const DB_PASS = 'Vvn#$iA3fHVkt8';
 const DB_NAME = 'userDB';
-
 ?> 
--- student_home.php
+++ student_home.php
@@ -18,7 +18,7 @@
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             <a class="nav-link" href="#">Sign out</a>
-        </li>z
+        </li>
     </ul>
 </header>
 <div id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">

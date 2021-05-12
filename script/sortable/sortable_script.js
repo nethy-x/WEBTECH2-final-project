b8676f28c8bb0b411a734fb4385d4bd2e517925b
Blochin <sevcik.lubomir02@gmail.com>
2021-05-12 16:11:31 +0200

    get elements question type 3

---
+++ script/sortable/sortable_script.js
@@ -0,0 +1,5 @@
+left_ansers = document.getElementsByClassName("left_answer");
+right_answers = document.getElementsByClassName("right_answer");
+
+console.log(left_ansers);
+console.log(right_answers);

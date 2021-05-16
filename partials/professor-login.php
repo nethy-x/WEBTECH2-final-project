55187b6237dfa23c1d49c9c68b7e3b548b655dff
Juraj Lapčák <55839515+georgeHeishi@users.noreply.github.com>
2021-05-16 15:13:25 +0200

    Mala zmena css
    Mala zmena css

--- partials/professor-login.php
+++ partials/professor-login.php
@@ -1,35 +1,37 @@
 <form action="api/loginProfessor.php" method="post">
     <div class="row mb-2">
         <div class=" d-flex justify-content-center">
-            <div class="p-2"><a href="index.php">Študent</a></div>
-            <div class="p-2"><a href="index.php?role=professor">Učiteľ</a></div>
+            <div class="p-2"><a href="index.php" class="page_link">Študent</a></div>
+            <div class="p-2"><a href="index.php?role=professor" class="page_link" style="color: yellow; text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; background-color: grey;">Učiteľ</a></div>
         </div>
     </div>
-    <div class="container">
+    <div class="container mt-5">
+        <hr>
         <div class="row mb-2">
             <div class="form-group col">
-                <label for="email"><b>Email</b></label>
+                <label for="email" class="col-form-label"><b>Email</b></label>
             </div>
-            <div class="form-group col">
-                <input type="text" id="email" placeholder="Vložte email" name="email" required>
+            <div class="form-group col-lg-8">
+                <input class="form-control" type="text" id="email" placeholder="Vložte email" name="email" required>
             </div>
         </div>
         <div class="row mb-2">
             <div class="form-group col">
-                <label for="psw"><b>Heslo</b></label>
+                <label for="psw" class="col-form-label"><b>Heslo</b></label>
             </div>
-            <div class="form-group col">
+            <div class="form-group col-lg-8">
                 <div class="form-group col">
-                    <input type="password" id="psw" placeholder="Vložte heslo" name="psw" required>
+                    <input class="form-control" type="password" id="psw" placeholder="Vložte heslo" name="psw" required>
                 </div>
             </div>
         </div>
+        <hr>
         <div class="row mb-2">
 
-            <button type="submit" class="btn btn-secondary mt-3">Prihlásiť sa</button>
+            <button type="submit" class="btn btn-secondary mt-5">Prihlásiť sa</button>
         </div>
     </div>
     <div class="row mb-2">
-        <p>Ak nie ste zaregistrovaný, registrujte sa na odkaze: <a href="registration.php">Registrácia</a></p>
+        <p>Ak nie ste zaregistrovaný, registrujte sa na odkaze: <a href="registration.php" class="reglog">Registrácia</a></p>
     </div>
 </form>
--- partials/student-login.php
+++ partials/student-login.php
@@ -1,46 +1,48 @@
 <form action="api/loginStudent.php" method="post">
     <div class="row mb-2">
         <div class="d-flex justify-content-center">
-            <div class="p-2"><a href="index.php">Študent</a></div>
-            <div class="p-2"><a href="index.php?role=professor">Učiteľ</a></div>
+            <div class="p-2"><a href="index.php" class="page_link" style="color: yellow; text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; background-color: grey;">Študent</a></div>
+            <div class="p-2"><a href="index.php?role=professor" class="page_link">Učiteľ</a></div>
         </div>
     </div>
-    <div class="container">
+    <div class="container mt-5">
+        <hr>
         <div class="row mb-2">
             <div class="form-group col">
-                <label for="code"><b>Kód testu</b></label>
+                <label for="code" class="col-form-label"><b>Kód testu</b></label>
             </div>
-            <div class="form-group col">
-                <input type="text" id="code" placeholder="Vložte kód testu" name="code" required>
+            <div class="form-group col-lg-7">
+                <input class="form-control" type="text" id="code" placeholder="Vložte kód testu" name="code" required>
             </div>
         </div>
         <div id="login" style="display: none;">
             <div class="row mb-2">
                 <div class="form-group col">
-                    <label for="name"><b>Meno</b></label>
+                    <label for="name" class="col-form-label"><b>Meno</b></label>
                 </div>
-                <div class="form-group col">
-                    <input type="text" id="name" placeholder="Vložte meno" name="name" required>
-                </div>
-            </div>
-            <div class="row mb-2">
-                <div class="form-group col">
-                    <label for="surname"><b>Priezvisko</b></label>
-                </div>
-                <div class="form-group col">
-                    <input type="text" id="surname" placeholder="Vložte priezvisko" name="surname" required>
+                <div class="form-group col-lg-7">
+                    <input class="form-control" type="text" id="name" placeholder="Vložte meno" name="name" required>
                 </div>
             </div>
             <div class="row mb-2">
                 <div class="form-group col">
-                    <label for="id_num"><b>Identifikačné číslo</b></label>
+                    <label for="surname" class="col-form-label"><b>Priezvisko</b></label>
                 </div>
-                <div class="form-group col">
-                    <input type="number" id="id_num" placeholder="Vložte identifikačné číslo" name="id_num" required>
+                <div class="form-group col-lg-7">
+                    <input class="form-control" type="text" id="surname" placeholder="Vložte priezvisko" name="surname" required>
                 </div>
             </div>
             <div class="row mb-2">
-                <button type="submit" class="btn btn-secondary mt-3">Prihlásiť sa</button>
+                <div class="form-group col">
+                    <label for="id_num" class="col-form-label"><b>Identifikačné číslo</b></label>
+                </div>
+                <div class="form-group col-lg-7">
+                    <input class="form-control" type="number" id="id_num" placeholder="Vložte identifikačné číslo" name="id_num" required>
+                </div>
+            </div>
+            <hr>
+            <div class="row mb-2">
+                <button type="submit" class="btn btn-secondary mt-5">Prihlásiť sa</button>
             </div>
         </div>
     </div>
